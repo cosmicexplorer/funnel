27d7d2e128f881435b641a691f0098654b6eb52b
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-08-26 14:34:59 -0400

    add cargo workspace

---
+++ language/src/lib.rs
@@ -0,0 +1,63 @@
+/*
+ * Description: ???
+ *
+ * Copyright (C) 2023 Danny McClanahan <dmcC2@hypnicjerk.ai>
+ * SPDX-License-Identifier: LGPL-3.0
+ *
+ * FIXME: is this sufficient license notice?
+ * Licensed under the Lesser GPL, Version 3.0 (see LICENSE).
+ */
+
+//! ???
+
+/* These clippy lint descriptions are purely non-functional and do not affect the functionality
+ * or correctness of the code. */
+// #![warn(missing_docs)]
+
+/* TODO: these can be removed, i guess... */
+#![feature(let_chains)]
+/* Note: run clippy with: rustup run nightly cargo-clippy! */
+#![deny(unsafe_code)]
+/* Ensure any doctest warnings fails the doctest! */
+#![doc(test(attr(deny(warnings))))]
+/* Enable all clippy lints except for many of the pedantic ones. It's a shame this needs to be
+ * copied and pasted across crates, but there doesn't appear to be a way to include inner
+ * attributes from a common source. */
+#![deny(
+  clippy::all,
+  clippy::default_trait_access,
+  clippy::expl_impl_clone_on_copy,
+  clippy::if_not_else,
+  clippy::needless_continue,
+  clippy::single_match_else,
+  clippy::unseparated_literal_suffix,
+  clippy::used_underscore_binding
+)]
+/* It is often more clear to show that nothing is being moved. */
+#![allow(clippy::match_ref_pats)]
+/* Subjective style. */
+#![allow(
+  clippy::derived_hash_with_manual_eq,
+  clippy::len_without_is_empty,
+  clippy::redundant_field_names,
+  clippy::too_many_arguments,
+  clippy::single_component_path_imports,
+  clippy::double_must_use
+)]
+/* Default isn't as big a deal as people seem to think it is. */
+#![allow(clippy::new_without_default, clippy::new_ret_no_self)]
+/* Arc<Mutex> can be more clear than needing to grok Orderings. */
+#![allow(clippy::mutex_atomic)]
+
+pub fn add(left: usize, right: usize) -> usize { left + right }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn it_works() {
+    let result = add(2, 2);
+    assert_eq!(result, 4);
+  }
+}
